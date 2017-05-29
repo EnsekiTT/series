865a1c841313eb4934635ff3b725ba59b91aad49
EnsekiTT <ensekitt@gmail.com>
2017-05-30 07:19:49 +0900

    単純移動平均をListでやった

--- google_finance.py
+++ google_finance.py
@@ -2,15 +2,34 @@ import pandas_datareader.data as web
 import datetime
 import matplotlib.pyplot as plt
 
+"""
+過去2年のデータから翌週を予想する
+"""
+
+TRAIN_DAYS = 600
+TEST_DAYS = 7
+
 train_start = datetime.datetime(2010, 1, 1)
-train_end = datetime.datetime(2013, 12, 31)
+train_end = train_start+datetime.timedelta(days=TRAIN_DAYS)
+test_start = train_end+datetime.timedelta(days=1)
+test_end = test_start+datetime.timedelta(days=TEST_DAYS)
 
-test_start = datetime.datetime(2014, 1, 1)
-test_end = datetime.datetime(2014, 12, 31)
+train = web.DataReader("F", 'google', train_start, train_end)
+test = web.DataReader("F", 'google', test_start, test_end)
 
-f = web.DataReader("F", 'google', train_start, train_end)
-v = web.DataReader("F", 'google', test_start, test_end)
-
-plt.plot(f.Close)
-plt.plot(v.Close)
+"""
+単純移動平均
+r1,r2,r3,
+         p1,
+   r2,r3,p1,
+            p2
+"""
+start = train[-20:].Close.data
+test_list = [i for i in start]
+for i in range(TEST_DAYS):
+    test_list.append(sum(test_list)/len(test_list))
+    test_list.pop(0)
+print(test_list)
+plt.plot(train.Close)
+plt.plot(test.Close)
 plt.show()
